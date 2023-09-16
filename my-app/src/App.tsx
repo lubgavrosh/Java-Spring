c34adcf7f4e340201bcdebb3573f9078fe4dc802
Liubomyr Havrosh <lubgavrosh@gmail.com>
2023-09-16 17:59:09 +0200

    security configurations has done

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -7,29 +7,25 @@ import CategoryEditPage from "./components/admin/category/edit/CategoryEditPage.
 import ProductListPage from "./components/admin/product/list/ProductListPage.tsx";
 import ProductCreatePage from "./components/admin/product/create/ProductCreatePage.tsx";
 import HomePage from "./components/admin/home/HomePage.tsx";
+import LoginPage from "./components/admin/auth/login/LoginPage.tsx";
 
 function App() {
 
     return (
         <>
             <Routes>
-                <Route path="/home" element={<DefaultLayout/>}>
+                <Route path={"/"} element={<DefaultLayout/>}>
                     <Route index element={<HomePage/>}/>
-                </Route>
-
-
-
-
-           <Route path="/category" element={<DefaultLayout/>}>
-                <Route index element={<CategoryListPage/>}/>
-                <Route path={"create"} element={<CategoryCreatePage/>}/>
-               <Route path={"edit/:id"} element={<CategoryEditPage/>}/>
-            </Route>
-
-
-                <Route path="/products" element={<DefaultLayout/>}>
-                    <Route index element={<ProductListPage/>}/>
-                    <Route path={"create"} element={<ProductCreatePage/>}/>
+                    <Route path={"category"}>
+                        <Route index element={<CategoryListPage/>}/>
+                        <Route path={"create"} element={<CategoryCreatePage/>}/>
+                        <Route path={"edit/:id"} element={<CategoryEditPage/>}/>
+                    </Route>
+                    <Route path={"products"}>
+                        <Route index element={<ProductListPage/>}/>
+                        <Route path={"create"} element={<ProductCreatePage/>}/>
+                   </Route>
+                    <Route path={"login"} element={<LoginPage />}></Route>
 
 
                 </Route>
--- my-app/src/components/containers/default/DefaultHeader.tsx
+++ my-app/src/components/containers/default/DefaultHeader.tsx
@@ -41,9 +41,9 @@ const DefaultHeader = () => {
                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
                                 </li>
                                 <li>
-                                    <a href="#"
+                                    <Link  to={`/login`}
                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
-                                        out</a>
+                                        in</Link>
                                 </li>
                             </ul>
                         </div>
@@ -62,7 +62,7 @@ const DefaultHeader = () => {
                          id="navbar-user">
                         <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
-                                <Link  to={`/home`}
+                                <Link  to={`/`}
                                    className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                                    aria-current="page">Home</Link>
                             </li>
