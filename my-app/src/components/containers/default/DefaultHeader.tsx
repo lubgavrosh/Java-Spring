d27b381619d9a7fea4b1c434fe810ec01628a612
Liubomyr Havrosh <lubgavrosh@gmail.com>
2023-09-23 12:14:21 +0200

    update client interface

--- my-app/src/components/containers/default/DefaultHeader.tsx
+++ my-app/src/components/containers/default/DefaultHeader.tsx
@@ -19,8 +19,9 @@ const DefaultHeader = () => {
                                 id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
                                 data-dropdown-placement="bottom">
                             <span className="sr-only">Open user menu</span>
-                            <img className="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
-                                 alt="user photo"/>
+                            {!isAuth ? (  <img className="w-8 h-8 rounded-full" src="https://i.pinimg.com/originals/a0/4d/84/a04d849cf591c2f980548b982f461401.jpg"
+                                 alt="user photo"/>):( <img className="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
+                                                            alt="user photo"/>)}
                         </button>
                         <div
                             className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
@@ -72,6 +73,7 @@ const DefaultHeader = () => {
                             </svg>
                         </button>
                     </div>
+                    {!isAuth ? (
                     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
                          id="navbar-user">
                         <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
@@ -80,13 +82,24 @@ const DefaultHeader = () => {
                                    className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                                    aria-current="page">Home</Link>
                             </li>
+
+                        </ul>
+                    </div>
+                    ):(  <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+                              id="navbar-user">
+                        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                            <li>
+                                <Link  to={`/`}
+                                       className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                                       aria-current="page">Home</Link>
+                            </li>
                             <li>
                                 <Link  to={`/category`}
-                                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Category</Link>
+                                       className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Category</Link>
                             </li>
                             <li>
                                 <Link  to={`/products`}
-                                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Products</Link>
+                                       className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Products</Link>
                             </li>
                             <li>
                                 <a href="#"
@@ -97,7 +110,7 @@ const DefaultHeader = () => {
                                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
                             </li>
                         </ul>
-                    </div>
+                    </div>)}
                 </div>
             </nav>
         </header>
